00da6dc9d3d00d90f55b464ae985eddb005dbecb
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-10-01 09:52:46 +0300

    step 1: welcome controller

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 ### Hexlet tests and linter status:
+
+
+### A Task's Manager:
 [![Actions Status](https://github.com/BroCodeX/java-project-99/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/BroCodeX/java-project-99/actions)
+
+## About
+Web application for tasks control. You can create, update, change tasks and olso change issuers.
+
+## Run checkstyle
+
+```bash
+make lint
+```
+
+## Make JaCoCo Report
+
+```bash
+make report
+```
+
+## Launch the Test
+
+```bash
+make test
+```
+
+## Link to the Web-App
+
+
+
+
+## The Multiple Runtime Version Manager *asdf*
+
+On *nix and macOS to manage Java versions we recommend using asdf https://github.com/asdf-vm/asdf.
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 	jacoco
 	id("com.github.johnrengelman.shadow") version "8.1.1"
 	id("com.adarshr.test-logger") version "4.0.0"
+	id("io.freefair.lombok") version "8.4"
 }
 
 group = "hexlet.code"
@@ -39,13 +40,12 @@ dependencies {
 
 	implementation("org.apache.commons:commons-lang3:3.14.0")
 	implementation("org.apache.commons:commons-collections4:4.4")
-	implementation("com.puppycrawl.tools:checkstyle:10.17.0")
-
-	implementation("org.slf4j:slf4j-simple:2.0.16")
 
 	implementation("org.postgresql:postgresql:42.7.3")
 
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-devtools")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
