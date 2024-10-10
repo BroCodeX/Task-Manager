ea5b81f9f58980139101c7b1a7209625f7a65702
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-10-10 09:21:01 +0300

    step 6: CRUD working

--- app/src/main/java/hexlet/code/app/controller/api/StatusController.java
+++ app/src/main/java/hexlet/code/app/controller/api/StatusController.java
@@ -6,6 +6,7 @@ import hexlet.code.app.dto.StatusUpdateDTO;
 import hexlet.code.app.service.StatusService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -24,34 +25,39 @@ public class StatusController {
     @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<StatusDTO>> index(@RequestParam(defaultValue = "10") Integer limit) {
-
+        List<StatusDTO> statusDTOS = service.getAll(limit);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Total-Count", String.valueOf(statusDTOS.size()));
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(statusDTOS);
     }
 
     @GetMapping("/{id}")
     @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.OK)
     public StatusDTO show(@PathVariable long id) {
-
+        return service.show(id);
     }
 
     @PostMapping("")
     @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.CREATED)
     public StatusDTO create(@Valid @RequestBody StatusCreateDTO dto) {
-
+        return service.create(dto);
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.OK)
     public StatusDTO update(@Valid @RequestBody StatusUpdateDTO dto, @PathVariable long id) {
-
+        return service.update(dto, id);
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void destroy(@PathVariable long id) {
-
+        service.destroy(id);
     }
 }
--- app/src/main/java/hexlet/code/app/service/StatusService.java
+++ app/src/main/java/hexlet/code/app/service/StatusService.java
@@ -1,7 +1,36 @@
 package hexlet.code.app.service;
 
+import hexlet.code.app.dto.StatusCreateDTO;
+import hexlet.code.app.dto.StatusDTO;
+import hexlet.code.app.dto.StatusUpdateDTO;
+import hexlet.code.app.repository.StatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StatusService {
+    @Autowired
+    private StatusRepository repository;
+
+    public List<StatusDTO> getAll(int limit) {
+
+    }
+
+    public StatusDTO show(long id) {
+
+    }
+
+    public StatusDTO create(StatusCreateDTO dto) {
+
+    }
+
+    public StatusDTO update(StatusUpdateDTO dto, long id) {
+
+    }
+
+    public void destroy(long id) {
+
+    }
 }
