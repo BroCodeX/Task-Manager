4b9e0673e36219e5a2227228d67ffafbdf6eee4a
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-10-12 11:42:55 +0300

    need to push slug in init data somehow

--- app/src/main/java/hexlet/code/app/component/InitData.java
+++ app/src/main/java/hexlet/code/app/component/InitData.java
@@ -1,10 +1,13 @@
 package hexlet.code.app.component;
 
+import hexlet.code.app.dto.StatusCreateDTO;
 import hexlet.code.app.dto.UserCreateDTO;
 import hexlet.code.app.mapper.UserMapper;
 import hexlet.code.app.model.Status;
 import hexlet.code.app.repository.StatusRepository;
 import hexlet.code.app.repository.UserRepository;
+import hexlet.code.app.service.StatusService;
+import hexlet.code.app.service.TaskService;
 import hexlet.code.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -33,6 +36,9 @@ public class InitData implements ApplicationRunner {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private StatusService statusService;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         initUser();
@@ -52,16 +58,15 @@ public class InitData implements ApplicationRunner {
         List<String> slugs = List.of("draft", "to_review", "to_be_fixed", "to_publish", "published");
         List<String> titles = List.of("New", "In Progress", "On Testing", "Completed", "Blocked");
 
-        List<Status> statusList = titles.stream()
+        List<StatusCreateDTO> statusList = titles.stream()
                 .map(s -> {
-                    var status = new Status();
-                    status.setName(s);
-                    status.setSlug("draft");
-                    return status;
+                    var dto = new StatusCreateDTO();
+                    dto.setName(s);
+                    return dto;
                 })
                 .toList();
 
-        statusRepository.saveAll(statusList);
+        statusList.forEach(statusService::create);
         List<String> checkTitles = statusRepository.findAll().stream()
                 .map(Status::getName)
                 .toList();
